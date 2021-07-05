98516b7a0a37afa210c6e42abf1a761b5cc408ab
Syahru <syahrumurtadho1@gmail.com>
2021-07-05 22:01:05 +0700

    Perbaikan komponen input, penambahan komponen select & input password

--- js/landingpage.js
+++ js/landingpage.js
@@ -8,3 +8,58 @@ $.each(row, function () {
     $(this).height(maxh);
   });
 });
+// Select City Phone Code 
+$(".city-select").mousedown(function(e) { 
+	e.preventDefault();
+});
+$(".city-select").click(function(e) { 
+	$("#list-select").toggleClass('show');
+});
+var listoptions = $("#list-select").hasClass("option");
+
+$('#list-select .option').click(function () {
+  $("#citycode :selected").val($(this).data('option'));
+  $("#citycode option").html($(this).data('option'));
+  $('.phone-number input').focus();
+  $("#list-select").toggleClass('show');
+});
+// $(".phone-number").focusin(function(e) { 
+// 	$(".city-select").addClass('show');
+// });
+
+// End Select City Phone Code 
+
+// Input Password Eye 
+$(".showhide").click(function () {
+  $(this).toggleClass("show");
+  var input = $($(this).attr("toggle"));
+  if ($(this).hasClass("show")) {
+    $(this).html("visibility_off");
+  } else {
+    $(this).html("visibility");
+  }
+  if ($(".password input").attr("type") == "password") {
+    $(".password input").attr("type", "text");
+  } else {
+    $(".password input").attr("type", "password");
+  }
+});
+// End Input Password Eye 
+
+// Validate Input 
+$(".group input").on("keyup", function () {
+  let empty = false;
+  let invalid = false;
+
+  $(".group input").each(function () {
+    empty = $(this).val().length == 0;
+  });
+
+  if (empty) {
+    $(".btn-primary-auth").addClass("disabled");
+    $(".btn-primary-auth").attr("disabled", "disabled");
+  } else {
+    $(".btn-primary-auth").removeClass("disabled");
+    $(".btn-primary-auth").attr("disabled", false);
+  }
+});
